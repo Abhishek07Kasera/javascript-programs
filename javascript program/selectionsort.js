efe2f2fec78437b28cbf81c65d79507e3b6c1010
Abhishek Kasera <89686557+Abhishek07Kasera@users.noreply.github.com>
2021-10-07 23:44:04 +0530

    new program

---
+++ javascript program/selectionsort.js
@@ -0,0 +1,17 @@
+var arr=[29,72,98,13,87,66,52,51,36]
+function selectionSort(arr){
+  let min;
+  for(let i =0; i<arr.length; i++){
+    min=i;
+    for(let j = i+1; j<arr.length; j++){
+      if(arr[j]<arr[min]){
+        min=j
+      }
+    }
+    if(min!==i){
+      [arr[i],arr[min]]=[arr[min],arr[i]]
+    }
+  }
+  return arr;
+}
+console.log(selectionSort([29,72,98,13,87,66,52,51,36]))
